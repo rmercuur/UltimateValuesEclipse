d9c9eb556b9c2a40167a0d5ab17c03c3bba7b56e
Rijk <r.a.mercuur@tudelft.nl>
2018-08-29 16:41:43 +0200

    Added ability to simulate different initial conditions for norm

--- UltimateValuesEclipse/src/agents/NormativeAgent5.java
+++ UltimateValuesEclipse/src/agents/NormativeAgent5.java
@@ -16,6 +16,7 @@ public class NormativeAgent5 extends Agent {
 	List<Integer> seenRespondsAccepted;
 	List<Integer> seenRespondsRejected;
 	int initialAction;
+	int initialAcceptRate;
 	
 	public NormativeAgent5(int ID, boolean isProposer) {
 		super(ID,isProposer);
@@ -23,6 +24,7 @@ public class NormativeAgent5 extends Agent {
 		seenRespondsAccepted=new ArrayList<Integer>();
 		seenRespondsRejected=new ArrayList<Integer>();	
 		initialAction = Helper.getParams().getInteger("initialActionNorm");
+		initialAcceptRate = Helper.getParams().getInteger("initialAcceptRateNorm");
 		//0 = Human
 		//1 = mu:0PieSize
 		//2 = mu:0.25PieSize
@@ -53,16 +55,28 @@ public class NormativeAgent5 extends Agent {
 					seenRespondsAccepted.stream().mapToDouble(a -> a).max().getAsDouble()) /
 					2;}
 		else{//if no norm available
-			if(initialAction ==0){ 
-				demand = -10; //do the action first-round agents do
+			if(initialAction ==0){ //do the action first-round agents do
+				demand = -10; 
 				double mean = 0.5618 * Helper.getParams().getInteger("pieSize");
 				double sd = 0.1289 * Helper.getParams().getInteger("pieSize");
 				while(demand < 0 || demand > Helper.getParams().getInteger("pieSize")){
 					demand= RandomHelper.createNormal(mean, sd).nextInt();
 				}
 			}
-			if(initialAction ==1){ 
+			if(initialAction ==1){
+				demand= 0;				
+			}
+			if(initialAction ==2){ //do the action first-round agents do
+				demand= RandomHelper.createUniform(0,0.5*Helper.getParams().getInteger("pieSize")).nextInt();
+			}
+			if(initialAction ==3){ //do the action first-round agents do
 				demand= RandomHelper.createUniform(0,Helper.getParams().getInteger("pieSize")).nextInt();
+			}	
+			if(initialAction ==4){
+				demand= RandomHelper.createUniform(0.5*Helper.getParams().getInteger("pieSize"),Helper.getParams().getInteger("pieSize")).nextInt();				
+			}
+			if(initialAction ==5){
+				demand= 1000;
 			}
 		}
 		return demand;
@@ -73,10 +87,31 @@ public class NormativeAgent5 extends Agent {
 		boolean accept;
 		
 		if(seenDemands.isEmpty()){
-			double acceptRate;
-			double mean = 0.806;
-			double sd = 0.395;
-			acceptRate= RandomHelper.createNormal(mean, sd).nextDouble(); //NB: are extreme values a problem? don't thinks o
+			double acceptRate =0.0;
+			if(initialAcceptRate ==0){ //do the action first-round agents do
+
+				double mean = 0.806;
+				double sd = 0.395;
+				acceptRate= RandomHelper.createNormal(mean, sd).nextDouble(); //NB: are extreme values a problem? don't thinks o
+			}
+
+			
+			if(initialAcceptRate ==1){ //do the action first-round agents do
+				acceptRate= RandomHelper.createUniform(0,0.0).nextDouble();
+			}
+			
+			if(initialAcceptRate ==2){ //do the action first-round agents do
+				acceptRate= RandomHelper.createUniform(0,0.5).nextDouble();
+			}
+			if(initialAcceptRate ==3){ //do the action first-round agents do
+				acceptRate= RandomHelper.createUniform(0,1.0).nextDouble();
+			}
+			if(initialAcceptRate ==4){
+				acceptRate= RandomHelper.createUniform(0.75,1.0).nextDouble();
+			}
+			if(initialAcceptRate ==5){
+				acceptRate= RandomHelper.createUniform(1.0,1.0).nextDouble();
+			}
 			accept= RandomHelper.createUniform(0,1).nextDouble() <acceptRate;
 		}
 		else{
